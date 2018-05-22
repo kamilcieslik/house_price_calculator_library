897d49a45050206f08a07e7f947e69e0887399a7
kcieslik226138 <kcieslik226138@e-science.pl>
2018-05-22 06:38:09 +0200

    Update setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='house-price-calculator',
-    version='0.2',
+    version='1.1',
     packages=find_packages(exclude=['tests*']),
     setup_requires=['setuptools-git'],
     license='MIT',
